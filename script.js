e34bc153ee4ee90fb9e56df3612f326c6bae31af
HanekawasBf <remo28126@gmail.com>
2026-06-01 15:19:13 -0700

    Implementacion de nav var

---
+++ script.js
@@ -0,0 +1,7 @@
+const formulario = document.getElementById("formBusqueda");
+
+formulario.addEventListener("submit", function (event) { //en cuanto submit, la funcion event va a...
+    event.preventDefault(); //Esto es para que no se recargue la pag
+    const texto = document.getElementById()
+
+})
